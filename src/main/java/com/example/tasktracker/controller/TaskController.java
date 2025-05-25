98d1896a0d8c238586e32be7b8ee49a9702fd1c5
Troy Nelson <troynelson@Troys-MacBook-Pro.local>
2025-05-24 23:05:14 -0400

    Enable CORS for frontend

--- src/main/java/com/example/tasktracker/controller/TaskController.java
+++ src/main/java/com/example/tasktracker/controller/TaskController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 
 
 public class TaskController {
